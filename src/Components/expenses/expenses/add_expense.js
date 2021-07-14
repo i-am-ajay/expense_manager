a6ce49a970af3654f12adc582a0381e68badab28
i-am-ajay <ajaychandel.1987@gmail.com>
2021-07-14 06:35:28 +0530

    resolving error while adding expense

--- src/Components/expenses/expenses/add_expense.js
+++ src/Components/expenses/expenses/add_expense.js
@@ -2,16 +2,14 @@ import './add_expense.css'
 
 const AddExpense = props =>{
     return (
-        <div>
-            <div className="row">
-                <div className="col-3"><input type="date" value={props.expense_obj.expense_date} onChange={props.object_creater }/></div>
-                <div className="col-6"><input type="text" value={props.expense_obj.expense_type} onChange={props.object_creater } 
+        <div className="m-1">
+            <div className="row p-1">
+                <div className="col-3"><input id="e_date" className="form-control form-control-sm" type="date" value={props.expense_obj.expense_date} onChange={props.object_creater }/></div>
+                <div className="col-4"><input id="e_type" className="form-control form-control-sm" type="text" value={props.expense_obj.expense_type} onChange={props.object_creater } 
                     placeholder="Type of Expense"/></div>
-                <div className="col-3"><input type="text" value={props.expense_obj.expense} onChange={props.object_creater} 
+                <div className="col-3"><input id="exp" className="form-control form-control-sm" type="text" value={props.expense_obj.expense} onChange={props.object_creater} 
                     placeholder="Expense"/></div>
-            </div>
-            <div>
-                <button className="btn btn-primary" onClick={props.addExpenseHandler}>Add Expense</button>
+                <div className="col-2"><button className="btn btn-sm btn-outline-danger" onClick={props.add_handler}>Add Expense</button></div>
             </div>
         </div>
     );
--- src/Components/expenses/expenses/expense_manager.js
+++ src/Components/expenses/expenses/expense_manager.js
@@ -16,53 +16,61 @@ const ExpenseManager = props =>{
     // array of expense
     const [expenseObjArray, addExpenseObj] = useState([
                                                 {
-                                                    "expense-date":new Date(2021,7,12),
+                                                    "expense_date":new Date(2021,7,12),
                                                     "expense_type":"Cooker Expense",
                                                     "expense": 1800.0
                                                 },
                                                 {
-                                                    "expense-date":new Date(2019,7,1),
+                                                    "expense_date":new Date(2019,7,1),
                                                     "expense_type":"Laptop Expense",
                                                     "expense": 63000.0
                                                 }
                                             ]);
 
     
-    const [expenseComponentArray, addExpenseComponent] = useState(expenseObjArray.filter(e => e['expense-date'].getFullYear() == new Date().getFullYear()).map(
+    const [expenseComponentArray, addExpenseComponent] = useState(expenseObjArray.filter(e => e['expense_date'].getFullYear() == new Date().getFullYear()).map(
         element =>{
             console.log(element);
-            return <Expense expense_date = {element['expense-date']} expense_type = {element['expense_type']} 
+            return <Expense expense_date = {element['expense_date']} expense_type = {element['expense_type']} 
             expense={element['expense']}></Expense>
         }
     ));
 
     /** Methods to handle various operation */
     const setExpenseObjectHandler = event =>{
-        if(event.target.id === ""){
+        if(event.target.id === "e_date"){
             createExpense(prvState =>{
                 return {...prvState,"expense_date":event.target.value};
             })
         }
-        else if(event.target.id === ""){
+        else if(event.target.id === "e_type"){
             createExpense(prvState =>{
                 return {...prvState,"expense_type":event.target.value};
             })
         }
-        else if(event.target.id === ""){
+        else if(event.target.id === "exp"){
             createExpense( prvState =>{
                 return {...prvState,"expense":event.target.value};
             }) 
         }
     }
     const addExpenseHandler = event =>{
+        console.log(expenseObjArray);
         addExpenseObj(prvState =>{
             return [...prvState,expenseObj];
         });
         // if selected filter matches expense year then re render the expense on screen.
+        addExpenseComponent(prvState =>{
+            expenseObjArray.map(element =>{
+                console.log(element);
+                return <Expense expense_date = {element['expense_date']} expense_type = {element['expense_type']} 
+                expense={element['expense']}></Expense>
+            })
+        })
     }
    
     return (
-        <div className="container">
+        <div className="container p-1 m-2 mx-auto">
             <AddExpense expense_obj = {expenseObj} object_creater = {setExpenseObjectHandler} add_handler={addExpenseHandler}></AddExpense>
             <div>{expenseComponentArray}</div>
         </div>
